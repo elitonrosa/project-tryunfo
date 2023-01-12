3cf656699ee6bedd50aa7a98e2e229ca2e2d9be5
Eliton da Rosa <elitondr@live.com>
2023-01-12 00:49:22 -0300

    Requisito 02 finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,36 @@ import React from 'react';
 import Form from './components/Form';
 
 class App extends React.Component {
+  onInputChange = () => {
+
+  };
+
+  onSaveButtonClick = () => {
+
+  };
+
   render() {
+    const infos = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
+        <Form
+          { ...infos }
+          onInputChange={ this.onInputChange }
+          onSaveButtonClick={ this.onSaveButtonClick }
+        />
       </div>
     );
   }
